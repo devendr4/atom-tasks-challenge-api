d200478fc64baa10ac7bfbdd659a00cd65b7b50b
devendr4 <angel.emo.altuve@gmail.com>
2023-08-30 21:30:53 -0400

    firebase install

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
 
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+
 // For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+initializeApp();
+
+const db = getFirestore();
+
 app.get("/", (req: Request, res: Response) => {
   console.log("test");
   res.json({ msg: "hello wlrd" });
 });
 
-app.get("/tasks", (req: Request, res: Response) => {
-  console.log("test");
-  res.json({ tasks: "tasks" });
+app.get("/tasks", async (req: Request, res: Response) => {
+  const snapshot = await db.collection("tasks").get();
+  console.log(snapshot);
+  res.json({ tasks: snapshot });
 });
 
 app.listen(port, () => {
